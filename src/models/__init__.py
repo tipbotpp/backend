b45d9bf0a6c1d0c5b051fba856a2217ec1832777
илья кинке <ilya@qu1nqqy.ru>
2026-03-13 11:25:31 +0300

    feat(models): добавил импорт модели Users в инициализацию моделей

---
+++ src/models/__init__.py
@@ -0,0 +1,19 @@
+from src.models.alert_settings import AlertSettings
+from src.models.balance_transactions import BalanceTransactions
+from src.models.donations import Donations
+from src.models.passive_income_settings import PassiveIncomeSettings
+from src.models.stop_words import StopWords
+from src.models.stream_goals import StreamGoals
+from src.models.stream_sessions import StreamSessions
+from src.models.users import Users
+
+__all__ = [
+	"AlertSettings",
+	"BalanceTransactions",
+	"Donations",
+	"PassiveIncomeSettings",
+	"StopWords",
+	"StreamGoals",
+	"StreamSessions",
+	"Users",
+]
