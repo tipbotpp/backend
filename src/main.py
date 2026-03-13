240e23efacfba225d657782e4f519d68422c91ea
илья кинке <ilya@qu1nqqy.ru>
2026-03-13 11:26:01 +0300

    feat(api): добавил настройку интеграции Dishka в приложение

--- src/main.py
+++ src/main.py
@@ -1,25 +1,28 @@
+"""Файл для определения точки входа в приложение и его базовой конфигурации"""
 from typing import Any, cast
 
+from dishka.integrations.fastapi import setup_dishka
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi.middleware.gzip import GZipMiddleware
 
-from src.core.config import cfg
+from src.api import router_v1
+from src.core.configs import cfg
 from src.core.exc.handlers import ExceptionHandlers
 from src.core.middlewares.logging import HTTPLoggingMiddleware
+from src.di.container import get_container
 from src.lifespan import lifespan
 
+# ========== App ==========
 app = FastAPI(lifespan=lifespan, title=cfg.app.title)
 
-# Exception handlers
-ExceptionHandlers.register(app)
-
-# Routers
-from src.api import router_v1
-
+# ========== Routers ==========
 app.include_router(router_v1)
 
-# Middlewares
+# ========== Exception handlers ==========
+ExceptionHandlers.register(app)
+
+# ========== Middlewares ==========
 app.add_middleware(cast(Any, HTTPLoggingMiddleware))
 app.add_middleware(
 	cast(Any, CORSMiddleware),
@@ -29,6 +32,10 @@ app.add_middleware(
 	allow_origins=cfg.app.allow_origins,
 )
 app.add_middleware(cast(Any, GZipMiddleware))
+
+# ========== DI ==========
+container = get_container()
+setup_dishka(container, app=app)
 
 
 def main() -> None:
