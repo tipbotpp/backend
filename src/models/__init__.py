1dfd490fb5a1e78e1469486bd843615d91f8740a
илья кинке <ilya@qu1nqqy.ru>
2026-03-23 16:10:33 +0300

    fix(models): добавил импорт Base в модельные определения

--- src/models/__init__.py
+++ src/models/__init__.py
@@ -1,3 +1,4 @@
+from src.models.base import Base
 from src.models.alert_settings import AlertSettings
 from src.models.balance_transactions import BalanceTransactions
 from src.models.donations import Donations
@@ -8,6 +9,7 @@ from src.models.stream_sessions import StreamSessions
 from src.models.users import Users
 
 __all__ = [
+	"Base",
 	"AlertSettings",
 	"BalanceTransactions",
 	"Donations",
