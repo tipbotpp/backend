4c94e67bf083d9994e3bd76543e7cdb9f7ef4035
илья кинке <ilya@qu1nqqy.ru>
2026-03-23 16:10:21 +0300

    feat(api): добавил маршрутизаторы для Auth, Users и JWKS

--- src/api/v1/endpoints/__init__.py
+++ src/api/v1/endpoints/__init__.py
@@ -1,3 +1,11 @@
 from fastapi import APIRouter
 
-routers: dict[str, APIRouter] = {}
+from src.api.v1.endpoints.auth import router as auth_router
+from src.api.v1.endpoints.jwks import router as jwks_router
+from src.api.v1.endpoints.users import router as users_router
+
+routers: dict[str, APIRouter] = {
+	"Auth": auth_router,
+	"Users": users_router,
+	"JWKS": jwks_router,
+}
