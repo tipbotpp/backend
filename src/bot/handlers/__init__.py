df25c09a7fb1a08c4a5617a62fda9a3d913eb859
илья кинке <ilya@qu1nqqy.ru>
2026-03-24 19:57:44 +0300

    refactor(bot): изменил импорт маршрутизатора start на новый путь

--- src/bot/handlers/__init__.py
+++ src/bot/handlers/__init__.py
@@ -1,6 +1,6 @@
 from aiogram import Router
 
-from .start import build_start_router
+from src.bot.handlers.start import router as start_router
 
 router = Router(name="main_router")
-router.include_router(build_start_router())
+router.include_router(start_router)
