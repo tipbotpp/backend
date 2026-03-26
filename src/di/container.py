a86bce02dcaf0c089b317ce31428f2bd7cd63b54
илья кинке <ilya@qu1nqqy.ru>
2026-03-26 23:04:06 +0300

    feat(di): добавил AuthProvider в контейнер зависимостей

--- src/di/container.py
+++ src/di/container.py
@@ -1,5 +1,7 @@
 from dishka import AsyncContainer, make_async_container
+from dishka.integrations.fastapi import FastapiProvider
 
+from .providers.auth import AuthProvider
 from .providers.core import CoreProvider, RequestProvider
 from .providers.repos import RepositoryProvider
 from .providers.services import ServiceProvider
@@ -7,8 +9,10 @@ from .providers.services import ServiceProvider
 
 def get_container() -> AsyncContainer:
     return make_async_container(
+        FastapiProvider(),
         CoreProvider(),
         RequestProvider(),
         RepositoryProvider(),
         ServiceProvider(),
+        AuthProvider(),
     )
