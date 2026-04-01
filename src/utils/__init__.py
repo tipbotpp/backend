fd6bf957c40e13137e68de1e162ad73949808c88
илья кинке <ilya@qu1nqqy.ru>
2026-04-02 01:24:13 +0300

    refactor(utils): изменил импорт local_time в модуле utils

--- src/utils/__init__.py
+++ src/utils/__init__.py
@@ -1,5 +1,5 @@
 from src.utils.cache import async_ttl_cache
-from src.utils.local_time import DateTimeLocal
+from src.utils import local_time
 from src.utils.mappers import map_model, map_value
 
-__all__ = ["async_ttl_cache", "DateTimeLocal", "map_model", "map_value"]
+__all__ = ["async_ttl_cache", "local_time", "map_model", "map_value"]
