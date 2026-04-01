cc8500df7781e3a3e8016fe20da4c3c18fb10e9d
илья кинке <ilya@qu1nqqy.ru>
2026-04-02 01:25:47 +0300

    feat(services): добавил методы получения AuthService и BalanceService

--- src/di/providers/services.py
+++ src/di/providers/services.py
@@ -1,5 +1,19 @@
-from dishka import Provider, Scope
+from redis.asyncio import Redis
+from sqlalchemy.ext.asyncio import AsyncSession
+
+from dishka import Provider, Scope, provide
+
+from src.services.auth import AuthService
+from src.services.balance import BalanceService
 
 
 class ServiceProvider(Provider):
 	scope = Scope.REQUEST
+
+	@provide
+	def get_auth_service(self, session: AsyncSession, redis: Redis) -> AuthService:
+		return AuthService(session, redis)
+
+	@provide
+	def get_balance_service(self, session: AsyncSession) -> BalanceService:
+		return BalanceService(session)
