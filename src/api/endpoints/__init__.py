f2de76a55bb20f29ea6f83f72315cd5d92a3c9a6
илья кинке <ilya@qu1nqqy.ru>
2026-04-05 01:07:54 +0300

    feat(api): добавил маршрутизатор для пожертвований

--- src/api/endpoints/__init__.py
+++ src/api/endpoints/__init__.py
@@ -2,6 +2,7 @@ from fastapi import APIRouter
 
 from src.api.endpoints.auth import router as auth_router
 from src.api.endpoints.balance import router as balance_router
+from src.api.endpoints.donations import router as donations_router
 from src.api.endpoints.health import router as health_router
 from src.api.endpoints.jwks import router as jwks_router
 from src.api.endpoints.users import router as users_router
@@ -10,6 +11,7 @@ routers: dict[str, APIRouter] = {
 	"Auth": auth_router,
 	"Users": users_router,
 	"Balance": balance_router,
+	"Donations": donations_router,
 	"JWKS": jwks_router,
 	"Health": health_router,
 }
