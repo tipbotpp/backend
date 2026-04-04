92d9ca3e502c4439a055f86b2470ee20ff1ef54a
илья кинке <ilya@qu1nqqy.ru>
2026-04-05 01:08:30 +0300

    feat(donations): добавил описание для параметра amount в DonationBody

--- src/schemas/pydantic/donations.py
+++ src/schemas/pydantic/donations.py
@@ -2,6 +2,8 @@ from __future__ import annotations
 
 from datetime import datetime
 
+from pydantic import Field
+
 from src.schemas.pydantic.common import (
 	BaseSchema,
 	Paginated,
@@ -11,7 +13,7 @@ from src.schemas.pydantic.common import (
 
 class DonationBody(BaseSchema):
 	streamer_id: int
-	amount: int
+	amount: int = Field(gt=0, description="Сумма доната в монетах")
 	message: str | None = None
 
 
