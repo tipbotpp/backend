f1ba8b0de301429fc40312f2ac02f8dc6924bb25
илья кинке <ilya@qu1nqqy.ru>
2026-04-09 12:27:05 +0300

    refactor(di): удалил RequestProvider из контейнера

--- src/di/container.py
+++ src/di/container.py
@@ -2,7 +2,7 @@ from dishka import AsyncContainer, make_async_container
 from dishka.integrations.fastapi import FastapiProvider
 
 from .providers.auth import AuthProvider
-from .providers.core import CoreProvider, RequestProvider
+from .providers.core import CoreProvider
 from .providers.repos import RepositoryProvider
 from .providers.services import ServiceProvider
 
@@ -11,7 +11,6 @@ def get_container() -> AsyncContainer:
     return make_async_container(
         FastapiProvider(),
         CoreProvider(),
-        RequestProvider(),
         RepositoryProvider(),
         ServiceProvider(),
         AuthProvider(),
