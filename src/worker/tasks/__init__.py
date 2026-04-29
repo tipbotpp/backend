7a716f01b8ea7c1ba5578f297b5681d2392a2dcc
илья кинке <ilya@qu1nqqy.ru>
2026-04-29 15:55:35 +0300

    feat(tasks): добавил passive_income_task в список импортируемых задач

--- src/worker/tasks/__init__.py
+++ src/worker/tasks/__init__.py
@@ -1,6 +1,4 @@
 from src.worker.tasks.donations import process_donation_media
+from src.worker.tasks.passive_income import passive_income_task
 
-# TODO: добавить passive_income_task — периодически начислять монеты зрителям
-#       активных стримов согласно PassiveIncomeSettings (coins_per_interval / interval_minutes)
-
-__all__ = ["process_donation_media"]
+__all__ = ["process_donation_media", "passive_income_task"]
