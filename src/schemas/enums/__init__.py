81380b6a49fdc6a0e1a51013ba472dfb18159b9f
илья кинке <ilya@qu1nqqy.ru>
2026-06-03 07:50:38 +0300

    feat: add tts enum

--- src/schemas/enums/__init__.py
+++ src/schemas/enums/__init__.py
@@ -1,5 +1,7 @@
 from .common import SortOrder
+from .tts import TTSVoice
 
 __all__ = [
 	"SortOrder",
+	"TTSVoice",
 ]
---
+++ src/schemas/enums/tts.py
@@ -0,0 +1,10 @@
+from enum import StrEnum
+
+
+class TTSVoice(StrEnum):
+	AIDAR = "aidar"
+	BAYA = "baya"
+	KSENIYA = "kseniya"
+	XENIA = "xenia"
+	EUGENE = "eugene"
+	RANDOM = "random"
